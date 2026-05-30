b146b72a4dab324d4dadda22542accc32abf4011
Augusto Fogaça <98088769+cuaitz@users.noreply.github.com>
2026-05-30 13:59:18 -0300

    [FEAT] Base do projeto

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// mongo
+console.log('Connecting to MongoDB...');
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Listening on http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+    process.exit(1);
+});
+
+// rotas
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
